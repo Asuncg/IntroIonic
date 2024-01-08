0800591da8647db35c131645f8999b7185c88814
Asun <ansuncg@gmail.com>
2024-01-08 13:32:34 +0100

    Primera subida proyecto Ionic de pruebas

--- src/app/pages/camera/camera.page.ts
+++ src/app/pages/camera/camera.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Camera, CameraResultType, ImageOptions } from '@capacitor/camera';
+import { Component, OnInit, ɵbypassSanitizationTrustResourceUrl } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, ImageOptions } from '@capacitor/camera';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 
@@ -10,23 +10,48 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 })
 export class CameraPage implements OnInit {
 
+  public image : any;
+
   private imageOptionsCamera: ImageOptions = {
     quality: 90,
     allowEditing: false,
-    resultType: CameraResultType.Uri
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    saveToGallery: true,
+  }
+
+  private imageOptionsGallery: ImageOptions = {
+    quality: 90,
+    allowEditing: false,
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Photos
   }
 
   constructor(private _storageService: LocalStorageService) { }
 
   ngOnInit() {
+    this.image = this._storageService.get("imagen")
   }
 
   async onClickCamera() {
+    const permisions = await Camera.checkPermissions();
+    if (permisions.camera) {
     const image = await Camera.getPhoto(this.imageOptionsCamera);
-    this._storageService.set("imagen", image.webPath)
+    this.image = this._storageService.set("imagen", image.webPath);
+    this.image = image.webPath;
+  } else {
+    console.log("No tienes acceso a la cámara")
   }
+}
 
-  onClickGallery() {
-
+ async onClickGallery() {
+    const permisions = await Camera.checkPermissions();
+    if (permisions.photos) {
+    const image = await Camera.getPhoto(this.imageOptionsGallery);
+    this.image = this._storageService.set("imagen", image.webPath);
+    this.image = image.webPath;
+  } else {
+    console.log("No tienes acceso a la galería")
+  }
   }
 }
